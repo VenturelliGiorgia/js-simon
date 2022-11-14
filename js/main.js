dd422ab84cd895f403824cb925f674bac74ce333
VenturelliGiorgia <venturelli.giorgia.03@gmail.com>
2022-11-14 20:28:29 +0100

    versione funzionante

---
+++ js/main.js
@@ -0,0 +1,53 @@
+const numeriRandom = document.getElementById("numeri-random");
+const secondi = 3;
+let arrayNumeriRandom = [];
+let numeriUtente = [];
+let numeriTrovati = [];
+let cont = 0;
+numeriRandomGenerator();
+
+function numeriRandomGenerator() {
+//genero 5 numeri random da 1 a 20
+    for (let i = 0; i < 5; i++) {
+        const num = Math.floor(Math.random() * 20) + 1;
+        console.log(num);
+        arrayNumeriRandom[i] = num;
+        numeriRandom.append(arrayNumeriRandom[i]);
+        numeriRandom.append(" ");
+    }
+    console.log(arrayNumeriRandom);
+}
+
+setTimeout(function () {
+
+    const containerNumeriRandom = document.getElementById("container-numeri-random");
+    containerNumeriRandom.classList.add("d-none");//vado a nasondere i numeri
+    numeriInseritiUtente();
+}, secondi * 1000);
+
+
+function numeriInseritiUtente() {
+
+    for (let i = 0; i < 5; i++) {
+        const num = prompt("inserisci in ordine i 5 numeri appena visti!");
+        numeriUtente[i] = num;
+
+        if (arrayNumeriRandom.includes(parseInt(num))) { //se questo numero è incluso nell'array vado a inserirlo nell'array numeriTrovati
+
+            numeriTrovati.push(num);
+            console.log(numeriTrovati);
+            cont++;
+        }
+    }
+
+    if (cont < 1) {
+        alert("Mi dispiace non hai indovinato nessun numero...");
+    } else if (cont === 1) {
+        alert("Bravo, hai inserito un numero corretto! ");
+    } else {
+        alert("Bravissimo, hai inserito " + cont + " numeri corretti");
+    }
+    console.log(numeriUtente);
+    console.log(numeriTrovati)
+
+}
